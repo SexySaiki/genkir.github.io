061f9268f4eb1109cadb839d6196362048a189c0
SexySaiki <30620930+SexySaiki@users.noreply.github.com>
2018-07-28 15:59:42 +0800

    fix a bit

--- j/c.js
+++ j/c.js
@@ -3,4 +3,4 @@ console.log("\n %c 520 Error %c サイキ <lolir.cn> ","color:#444;background:#e
 console.log('\n %c サイキ:我博客啥时候能恢复 %c \n', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-left-radius:5px;border-bottom-left-radius:5px;', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-right-radius:5px;border-bottom-right-radius:5px;');
 console.log('\n %c Shadow:今年之内 %c \n', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-left-radius:5px;border-bottom-left-radius:5px;', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-right-radius:5px;border-bottom-right-radius:5px;');
 console.log('\n %c 橙影带着小姨子跑了！ %c \n', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-left-radius:5px;border-bottom-left-radius:5px;', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-right-radius:5px;border-bottom-right-radius:5px;');
-console.log("%c\n     ","font-size:250px;background:url('https://source.kilin.moe/j/abc.gif') no-repeat 0 0");
+console.log("%c\n     ","font-size:240px;background:url('https://source.kilin.moe/j/abc.gif') no-repeat 0 0");

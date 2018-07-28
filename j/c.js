6dff692fc3910008782ab75b25268dde0f3bd5b4
SexySaiki <30620930+SexySaiki@users.noreply.github.com>
2018-07-28 15:43:44 +0800

    fix a bit

--- j/c.js
+++ j/c.js
@@ -1,5 +1,5 @@
 //这是一个奇怪的JS 你可能无法理解
-console.log('\n %c © サイキ | 求职欲望强烈 | lolir.cn %c \n', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-left-radius:5px;border-bottom-left-radius:5px;', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-right-radius:5px;border-bottom-right-radius:5px;');
+console.log("\n %c 520 Error %c サイキ <lolir.cn>","color:#444;background:#eee;padding:5px 0;","color:#eee;background:#444;padding:5px 0;");
 console.log('\n %c サイキ:我博客啥时候能恢复 %c \n', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-left-radius:5px;border-bottom-left-radius:5px;', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-right-radius:5px;border-bottom-right-radius:5px;');
 console.log('\n %c Shadow:今年之内 %c \n', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-left-radius:5px;border-bottom-left-radius:5px;', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-right-radius:5px;border-bottom-right-radius:5px;');
 console.log('\n %c 橙影带着小姨子跑了！ %c \n', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-left-radius:5px;border-bottom-left-radius:5px;', 'color:#455a64;background:#e0e0e0;padding:5px 0;border-top-right-radius:5px;border-bottom-right-radius:5px;');
